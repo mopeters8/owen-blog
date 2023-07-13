2d042e1d39e2b3abf5efc1408ae289f7382caabc
mopeters8 <mopeters8@gmail.com>
2023-07-12 17:56:07 -0700

    starting image + name intro

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -6,24 +6,43 @@ export default function About() {
 
   return (
     <div className="aboutPage">
-      <div className="biography">
-        <div className="bioHeadshot"></div>
-        <img
-          className="bioImg"
-          src="https://drive.google.com/uc?export=view&id=1wNuqXOVr_kSp2sQOdP2p66BsUasO4bK2"
-          alt=""
-        />
+      <div class="container bioHeadshot">
+        <div class="row">
+          <div class="col-sm-4 column">
+            <img
+              className="bioImg"
+              src="https://drive.google.com/uc?export=view&id=1wNuqXOVr_kSp2sQOdP2p66BsUasO4bK2"
+              alt=""
+            />
+          </div>
+          <div class="col-lg-8 column">
+            <p className="biographyText">
+              <p className="bioIntro">Hi, my name is</p>
+              <p className="bioName">Owen Peters.</p>
+              <p className="bioSubtext">I try to code web things.</p>
+              <p className="bioDesc">
+                I'm a software engineering with a strong interest in web design.
+                I am currently working on this website, and looking for my entry
+                into industry
+              </p>
+            </p>
+          </div>
+        </div>
+        <div class="row bioButtons">
+          <div class="col">
+            <button type="button" class="btn btn-outline-secondary">
+              View Portfolio
+            </button>
+            <button type="button" class="btn btn-outline-secondary">
+              View Rest of Site
+            </button>
+          </div>
+        </div>
       </div>
-      <div className="bio">
-        <p>Hi, my name is</p>
-        <p>Michael Owen Peters</p>
-        <p>
-          I'm a software engineering with a strong interest in web design. I am
-          currently working on this website, and looking for my entry into
-          industry
-        </p>
-        {/* <!-- emphasis on M and O --> */}
-      </div>
+      <hr class="rounded"></hr>
+
+      <div className="biography"></div>
+      <div className="bio">{/* <!-- emphasis on M and O --> */}</div>
     </div>
   );
 }
